13d4a3241f7dd7e4c6e7bf7fb1af1ac2f2179c09
burcuaydinn <burcuaydin2417@gmail.com>
2024-07-17 19:13:00 +0300

    store

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import store from "./app/store.js";
+import store from "./store/store.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
